6f87f8a87cf674c45740f5af040abecd62599b30
Kevin Finley <kftwotwo@gmail.com>
2016-12-01 23:23:53 -0800

    add teachers in html

--- app.rb
+++ app.rb
@@ -9,3 +9,14 @@ end
 get("/teachers/new") do
   erb(:new_teacher)
 end
+
+get('/teachers') do
+  @teachers = Teacher.all()
+  erb(:teachers)
+end
+
+post('/teachers') do
+  teacher = Teacher.new(:name => params[:name])
+  teacher.add()
+  erb(:success)
+end
